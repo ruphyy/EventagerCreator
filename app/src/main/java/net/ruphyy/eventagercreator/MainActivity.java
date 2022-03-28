5fc7dd7af1f4e84b77f53412cbd0bf659c0f455d
ruphyy <andremiguel@web.de>
2022-03-28 12:20:16 +0200

    Cleanup imports Added method "percent" in Main Worked at Frontend

--- app/src/main/java/net/ruphyy/eventagercreator/MainActivity.java
+++ app/src/main/java/net/ruphyy/eventagercreator/MainActivity.java
@@ -1,21 +1,26 @@
 package net.ruphyy.eventagercreator;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+    // TODO: Datenbankverknüpfung und Abruf der Daten
+    // TODO: NavigationBar state_selected Farben ändern
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView emptyView;
+    TextView emptyView, contactSupport;
     CardView homeView;
     RecyclerView recyclerView;
     String s1[], s2[];
@@ -31,14 +36,37 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.creatorEvents);
         emptyView = findViewById(R.id.emptyView);
         homeView = findViewById(R.id.homeView);
+        contactSupport = findViewById(R.id.contactSupport);
+        navigationView = findViewById(R.id.bottom_navigation);
 
         s1 = getResources().getStringArray(R.array.events);
         s2 = getResources().getStringArray(R.array.dates);
+
+        // Support kontaktieren per E-Mail
+        contactSupport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mailto = "mailto:support@eventager.com";
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                emailIntent.setData(Uri.parse(mailto));
+
+                // Öffnet E-Mail ansonsten Errortoast (Try-Catch um Absturz zu verhindern)
+                try {
+                    startActivity(emailIntent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(MainActivity.this, "Error to open email app", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         MyAdapter ma = new MyAdapter(this, s1, s2, images);
         recyclerView.setAdapter(ma);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        /* Wenn User keine Events erstellt hat
+        recyclerView (Liste) verstecken und TextView
+        mit Hinweis anzeigen lassen.
+         */
         if (s1.length == 0) {
             recyclerView.setVisibility(View.GONE);
             emptyView.setVisibility(View.VISIBLE);
@@ -47,7 +75,6 @@ public class MainActivity extends AppCompatActivity {
             recyclerView.setVisibility(View.VISIBLE);
             emptyView.setVisibility(View.GONE);
         }
-        navigationView = findViewById(R.id.bottom_navigation);
         navigationView.setOnNavigationItemSelectedListener
                 ((BottomNavigationView.OnNavigationItemSelectedListener) item -> {
                     switch (item.getItemId()) {
@@ -61,4 +88,18 @@ public class MainActivity extends AppCompatActivity {
                     return false;
                 });
     }
+
+    // Wird benutzt um prozentualen Anteil von Follower
+    public double percent(int x, int y) { // X von Y (= Insg.)
+        double result;
+
+        // 1 % berechnen
+        y = 100/y;
+        // Prozentualer Anteil von Y berechnen
+        result = y*x;
+        // Auf 2 Nachkommastellen runden
+        result = Math.round(result * 100.0) / 100.0;
+        return result;
+    }
+
 }
